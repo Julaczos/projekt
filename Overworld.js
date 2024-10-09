db17eeb8da54ede00818892a0afe639a81e9766c
Julaczos <julaczosoo@gmail.com>
2024-10-09 21:42:22 +0200

    Update Overworld.js

--- Overworld.js
+++ Overworld.js
@@ -3,11 +3,11 @@ class Overworld {
    this.element = config.element;
    this.canvas = this.element.querySelector(".game-canvas");
    this.ctx = this.canvas.getContext("2d");
+  this.map = null;
  }
 
  startGameLoop(){
   const step = () => {
-   console.log("it's working");
    requestAnimationFrame(() => {
      step();
     })
@@ -17,22 +17,9 @@ class Overworld {
 
  
  init() {
-
+  this.map = new OverworldMap(window.OverworldMap.DemoRoom);
   this.startGameLoop();
-   
-   const image = new Image();
-   image.onload = () => {
-     this.ctx.drawImage(image,0,0)
-   };
-   image.src = "/projekt/images/DemoLower.png";
-   const hero = new GameObject({
-     x: 5,
-     y: 6,
-   })
-   const npc1 = new GameObject({
-    x: 7,
-    y: 9,
-    src: "/projekt/images/npc1.png"
-  })
+
+  
  }
 }
