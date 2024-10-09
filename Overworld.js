3e8758c5903c598280455e57503acdf132ab002d
Julaczos <julaczosoo@gmail.com>
2024-10-09 21:56:19 +0200

    Update Overworld.js

--- Overworld.js
+++ Overworld.js
@@ -3,31 +3,31 @@ class Overworld {
    this.element = config.element;
    this.canvas = this.element.querySelector(".game-canvas");
    this.ctx = this.canvas.getContext("2d");
-  this.map = null;
+   this.map = null;
  }
-
- startGameLoop(){
-  const step = () => {
-
-   this.map.drawLowerImage(this.ctx);
-
-   Object.values(this.map.gameObjects).forEach(object => {
-    object.sprite.draw(this.ctx);
-   }
-   
-   this.map.drawUpperImage(this.ctx);
-   requestAnimationFrame(() => {
-     step();
-    })
-  }
-  step();
- }
-
  
+ startGameLoop() {
+   const step = () => {
+    
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    
+    this.map.drawLowerImage(this.ctx);
+    
+    Object.values(this.map.gameObjects).forEach(object => {
+      object.x += 0.02;
+      object.sprite.draw(this.ctx);
+    })
+    
+    this.map.drawUpperImage(this.ctx);
+     requestAnimationFrame(() => {
+      step();   
+     })
+    
+   }
+   step();
+ }
  init() {
-  this.map = new OverworldMap(window.OverworldMap.DemoRoom);
+  this.map = new OverworldMap(window.OverworldMaps.Kitchen);
   this.startGameLoop();
-
-  
  }
 }
