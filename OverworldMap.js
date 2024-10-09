2dc05266b2cb05692e1d73bd9f773a2c5af598ae
Julaczos <julaczosoo@gmail.com>
2024-10-09 21:55:07 +0200

    Update OverworldMap.js

--- OverworldMap.js
+++ OverworldMap.js
@@ -1,6 +1,6 @@
-class OverworldMap{
-  constructor (config){
-    this.gameObjcts = config.gameObjects;
+class OverworldMap {
+  constructor(config) {
+    this.gameObjects = config.gameObjects;
 
     this.lowerImage = new Image();
     this.lowerImage.src = config.lowerSrc;
@@ -9,13 +9,13 @@ class OverworldMap{
     this.upperImage.src = config.upperSrc;
   }
 
-  drawLowerImage(ctx){
+  drawLowerImage(ctx) {
     ctx.drawImage(this.lowerImage, 0, 0)
   }
 
-  drawUpperImage(ctx){
+  drawUpperImage(ctx) {
     ctx.drawImage(this.upperImage, 0, 0)
-  }
+  } 
 }
 
 window.OverworldMaps = {
@@ -27,13 +27,11 @@ window.OverworldMaps = {
         x: 5,
         y: 6,
       }),
-      npc1: new GameObject ({
+      npc1: new GameObject({
         x: 7,
         y: 9,
         src: "/projekt/images/npc1.png"
       })
     }
   }
-
-  
 }
