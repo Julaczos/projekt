d7b7a2d4282edda491289a37307cb8fbe2ccc86a
Julaczos <julaczosoo@gmail.com>
2024-10-09 14:58:14 +0200

    Create Sprite.js

---
+++ Sprite.js
@@ -0,0 +1,28 @@
+class Sprite {
+  constructor (config){
+
+    this.image = new Image();
+    this.image.src = config.src;
+    this.image.onload = () => {
+      this.isLoaded = true;
+    }
+    
+    this.animations = config.animations || {
+      idleDown: [
+        [0,0], [1,0], [2,0], [3,0]
+      ]
+    }
+    this.currentAnimation = config.currentAnimation || "idleDown";
+    this.currentAnimationFrame = 0;
+
+    this.gameObject = config.gameObject;
+  }
+
+
+  draw (ctx) {
+    const x = this.gameObject.x * 16;
+    const y = this.gameObject.y * 16;
+
+    ctx.drawImage(this.image,0,0,16,32,x,y,16,32)
+  }
+}
