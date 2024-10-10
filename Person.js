cf0d4600b19f220f17bab4e45421a4ea279e2fdd
Julaczos <julaczosoo@gmail.com>
2024-10-10 19:21:35 +0200

    Update Person.js

--- Person.js
+++ Person.js
@@ -1,13 +1,15 @@
-class Person extends GameObject{
-  constructor (config) {
+class Person extends GameObject {
+  constructor(config) {
     super(config);
     this.movingProgressRemaining = 0;
 
+    this.isPlayerControlled = config.isPlayerControlled || false;
+
     this.directionUpdate = {
-      "up": ["y",-1],
-      "down": ["y",1],
-      "left": ["x",-1],
-      "right": ["x",1],
+      "up": ["y", -1],
+      "down": ["y", 1],
+      "left": ["x", -1],
+      "right": ["x", 1],
     }
   }
 
@@ -19,12 +21,13 @@ class Person extends GameObject{
       this.movingProgressRemaining = 16;
     }
   }
-  
-  updatePosition (){
-    if (this.movingProgressRemaining > 0)  {
+
+  updatePosition() {
+    if (this.movingProgressRemaining > 0) {
       const [property, change] = this.directionUpdate[this.direction];
       this[property] += change;
       this.movingProgressRemaining -= 1;
     }
   }
+
 }
