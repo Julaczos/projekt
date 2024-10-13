5b261f94837ae8b4fddbd49ded1ef5413cf05460
Julaczos <julaczosoo@gmail.com>
2024-10-13 10:06:02 +0200

    Update TextMessage.js

--- TextMessage.js
+++ TextMessage.js
@@ -1,22 +1,38 @@
 class TextMessage {
-  constructor ({text, onComplete}) {
+  constructor({ text, onComplete }) {
     this.text = text;
     this.onComplete = onComplete;
     this.element = null;
   }
 
-  createElement (){
-    this.element = document.createElement ("div");
+  createElement() {
+    this.element = document.createElement("div");
     this.element.classList.add("TextMessage");
 
     this.element.innerHTML = (`
-      <p class="TextMessage_p">$(this.text)</p>
+      <p class="TextMessage_p">${this.text}</p>
       <button class="TextMessage_button">Next</button>
     `)
+
+    this.element.querySelector("button").addEventListener("click", () => {
+      this.done();
+    });
+
+    this.actionListener = new KeyPressListener("Enter", () => {
+      this.actionListener.unbind();
+      this.done();
+    })
+
   }
 
-  init (container) {
+  done() {
+    this.element.remove();
+    this.onComplete();
+  }
+
+  init(container) {
     this.createElement();
     container.appendChild(this.element)
   }
+
 }
