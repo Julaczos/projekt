a70f9470b76a71b0421a566599f00e592874dd20
Julaczos <julaczosoo@gmail.com>
2024-10-13 10:23:26 +0200

    Update Overworld.js

--- Overworld.js
+++ Overworld.js
@@ -21,7 +21,9 @@ class Overworld {
 
       this.map.drawLowerImage(this.ctx, cameraPerson);
 
-      Object.values(this.map.gameObjects).forEach(object => {
+      Object.values(this.map.gameObjects).sort((a,b) => {
+        return a.y - b.y;
+      }).forEach(object => {
         object.sprite.draw(this.ctx, cameraPerson);
       })
 
@@ -34,17 +36,50 @@ class Overworld {
     step();
  }
 
- init() {
-  this.map = new OverworldMap(window.OverworldMaps.MainMap);
+ bindActionInput() {
+   new KeyPressListener("Enter", () => {
+     this.map.checkForActionCutscene()
+   })
+ }
+
+ bindHeroPositionCheck() {
+   document.addEventListener("PersonWalkingComplete", e => {
+     if (e.detail.whoId === "hero") {
+       this.map.checkForFootstepCutscene()
+     }
+   })
+ }
+
+ startMap(mapConfig) {
+  this.map = new OverworldMap(mapConfig);
+  this.map.overworld = this;
   this.map.mountObjects();
+ }
+
+ init() {
+  this.startMap(window.OverworldMaps.DemoRoom);
+
+
+  this.bindActionInput();
+  this.bindHeroPositionCheck();
 
   this.directionInput = new DirectionInput();
   this.directionInput.init();
 
   this.startGameLoop();
 
-  this.map.startCutscene([
-   {type: "textMessage", text: "hello world"}
-  ])
+
+   this.map.startCutscene([
+  //   { who: "hero", type: "walk",  direction: "down" },
+  //   { who: "hero", type: "walk",  direction: "down" },
+  //   { who: "npcA", type: "walk",  direction: "up" },
+  //   { who: "npcA", type: "walk",  direction: "left" },
+  //   { who: "hero", type: "stand",  direction: "right", time: 200 },
+     { type: "textMessage", text: "WHY HELLO THERE!"}
+  //   // { who: "npcA", type: "walk",  direction: "left" },
+  //   // { who: "npcA", type: "walk",  direction: "left" },
+  //   // { who: "npcA", type: "stand",  direction: "up", time: 800 },
+  // ])
+
  }
 }
