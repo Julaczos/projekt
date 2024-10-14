ef3f6cd9c2e9f129ffa1b531789cf32e6c58f447
Julaczos <julaczosoo@gmail.com>
2024-10-15 00:16:34 +0200

    Update video.js

--- video.js
+++ video.js
@@ -1,20 +1,37 @@
+let isAudioPlaying = false; 
+
+
 function toggleVideo() {
     const video = document.getElementById("gameVideo");
     const closeButton = document.getElementById("closeVideoButton");
     const videoButton = document.getElementById("videoButton");
+    const audio = document.getElementById("backgroundAudio");
 
-    if (video.paused) {
+    if (video.style.display === "none") {
         video.style.display = "block";
-        closeButton.style.display = "none";
+        closeButton.style.display = "block";
+        videoButton.style.display = "none";
         video.play();
+
+        if (!audio.paused) {
+            audio.pause(); 
+        }
+
         videoButton.textContent = "Zamknij wideo"; 
     } else {
         video.pause();
         video.style.display = "none";
         closeButton.style.display = "none";
+        videoButton.style.display = "block";
+
+        if (isAudioPlaying) {
+            audio.play();
+        }
+
         videoButton.textContent = "Włącz wideo"; 
     }
 }
+
 
 document.getElementById("backgroundAudio").volume = 0.5;
 
@@ -24,9 +41,12 @@ function toggleAudio() {
 
     if (audio.paused) {
         audio.play();
+        isAudioPlaying = true; 
         audioButton.textContent = "Wyłącz audio";
     } else {
         audio.pause();
+        isAudioPlaying = false; 
         audioButton.textContent = "Włącz audio";
     }
 }
+
