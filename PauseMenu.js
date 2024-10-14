ee4ef8591f28ee8b5f634c180354a3f1ef70c8d7
Julaczos <julaczosoo@gmail.com>
2024-10-14 17:52:13 +0200

    Update PauseMenu.js

--- PauseMenu.js
+++ PauseMenu.js
@@ -37,6 +37,7 @@ class PauseMenu {
     this.element.classList.add("PauseMenu");
     this.element.innerHTML = (`
       <h2>Pauza</h2>
+      <div class="stats"></div> <!-- Placeholder dla statystyk -->
     `);
   }
 
@@ -48,18 +49,25 @@ class PauseMenu {
   }
 
   showStatistics() {
+    const existingStatsElement = this.element.querySelector(".statsOverlay");
+    if (existingStatsElement) {
+      existingStatsElement.remove();
+      return; 
+    }
+
     const statsElement = document.createElement("div");
     statsElement.classList.add("statsOverlay");
     statsElement.innerHTML = `
       <h3>Twoje Statystyki</h3>
-      <p>Poziom: ${window.level}</p>
-      <p>Doświadczenie: ${window.xp} / ${window.xpToNextLevel}</p>
-      <p>Przysiady: ${window.squatCount}</p>
-      <p>Biceps Curls: ${window.bicepCurlCount}</p>
+      <p>Poziom: ${window.GlobalStats.level}</p>
+      <p>Doświadczenie: ${window.GlobalStats.xp} / ${window.GlobalStats.xpToNextLevel}</p>
+      <p>Przysiady: ${window.GlobalStats.squatCount}</p>
+      <p>Biceps Curls: ${window.GlobalStats.bicepCurlCount}</p>
+      <p>Lokalizacja: ${window.GlobalStats.currentLocation}</p>
       <button id="closeStats">Zamknij</button>
     `;
-
-    document.body.appendChild(statsElement);
+    
+    this.element.appendChild(statsElement);
 
     document.getElementById("closeStats").addEventListener("click", () => {
       statsElement.remove();
