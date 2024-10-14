b560bd5f11fb2f03c21626f84cf465b236e55f34
Julaczos <julaczosoo@gmail.com>
2024-10-14 13:09:01 +0200

    Update video.js

--- video.js
+++ video.js
@@ -1,45 +1,17 @@
-       function playVideo() {
-            const video = document.getElementById("gameVideo");
-            const closeButton = document.getElementById("closeVideoButton");
-            
-            video.style.display = "block"; 
-            closeButton.style.display = "block"; 
-            video.play(); 
-        }
+function playVideo() {
+    const video = document.getElementById("gameVideo");
+    const closeButton = document.getElementById("closeVideoButton");
+    
+    video.style.display = "block"; 
+    closeButton.style.display = "block"; 
+    video.play(); 
+}
 
-        function closeVideo() {
-            const video = document.getElementById("gameVideo");
-            const closeButton = document.getElementById("closeVideoButton");
-            
-            video.pause(); 
-            video.style.display = "none"; 
-            closeButton.style.display = "none"; 
-        }
-
-        let isDragging = false;
-        let offsetX, offsetY;
-
-        const video = document.getElementById("gameVideo");
-
-        video.addEventListener("mousedown", (event) => {
-            isDragging = true;
-            offsetX = event.clientX - video.getBoundingClientRect().left;
-            offsetY = event.clientY - video.getBoundingClientRect().top;
-
-            document.addEventListener("mousemove", mouseMoveHandler);
-            document.addEventListener("mouseup", mouseUpHandler);
-        });
-
-        function mouseMoveHandler(event) {
-            if (isDragging) {
-                video.style.left = `${event.clientX - offsetX}px`;
-                video.style.top = `${event.clientY - offsetY}px`;
-            }
-        }
-
-        function mouseUpHandler() {
-            isDragging = false;
-
-            document.removeEventListener("mousemove", mouseMoveHandler);
-            document.removeEventListener("mouseup", mouseUpHandler);
-        }
+function closeVideo() {
+    const video = document.getElementById("gameVideo");
+    const closeButton = document.getElementById("closeVideoButton");
+    
+    video.pause(); 
+    video.style.display = "none"; 
+    closeButton.style.display = "none"; 
+}
