c086aff414ce0a8da19e110a463c4c73d01df2a9
Julaczos <julaczosoo@gmail.com>
2024-10-16 22:22:27 +0200

    Update enemies.js

--- enemies.js
+++ enemies.js
@@ -1,5 +1,19 @@
-const enemy1 = {
-  name: "Wrogi Tomasz",
-  hp: 40,
-  level: 1,
-};
+window.Enemies = {
+  "tomasz": {
+    name: "Tomasz",
+    src: "/projekt/images/npc12.png",
+    
+  },
+  "beth": {
+    name: "Beth",
+    src: "/images/characters/people/npc1.png",
+    pizzas: {
+      "a": {
+        hp: 1,
+        pizzaId: "f001",
+        maxHp: 50,
+        level: 1,
+      },
+    }
+  }
+}
