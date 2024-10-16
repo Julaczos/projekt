a1fa039ab7fdae451c79c7dbbe4d505f3ec5ea05
Julaczos <julaczosoo@gmail.com>
2024-10-16 20:27:06 +0200

    Update PlayerState.js

--- PlayerState.js
+++ PlayerState.js
@@ -1,19 +1,15 @@
 class PlayerState {
   constructor() {  
-    this.storyFlags = {
-      //Five_Squats: true
+    this.storyFlags = {};
+    this.playerStats = {
+      name: "player",
+      hp: 50,
+      maxHp: 50,
+      xp: 0,
+      maxXp: 100,
+      level: 1,
+      status: null,
     };
   }
 }
 window.playerState = new PlayerState();
-
-window.playerStats = {
-  name: "player",
-  hp: 50,
-  maxHp: 50,
-  xp: 0,
-  maxXp: 100,
-  level: 1,
-  status: null,
-};
-
