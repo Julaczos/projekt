321a53dc9aafab82327a396af14e3583249d0a87
Julaczos <julaczosoo@gmail.com>
2024-10-17 22:37:13 +0200

    123

--- battle/Battle.js
+++ battle/Battle.js
@@ -1,90 +1,130 @@
 class Battle {
   constructor({ enemy, onComplete }) {
+
     this.enemy = enemy;
     this.onComplete = onComplete;
 
-    this.combatants = {};
+    this.combatants = {
+    }
 
     this.activeCombatants = {
-      player: null,
-      enemy: null,
-    };
+      player: null, //"player1",
+      enemy: null, //"enemy1",
+    }
 
-    this.addCombatant("player", window.playerStats);
-    this.addCombatant("enemy", this.enemy);
+    //Dynamically add the Player team
+    window.playerState.lineup.forEach(id => {
+      this.addCombatant(id, "player", window.playerState.pizzas[id])
+    });
+    //Now the enemy team
+    Object.keys(this.enemy.pizzas).forEach(key => {
+      this.addCombatant("e_"+key, "enemy", this.enemy.pizzas[key])
+    })
 
-    this.items = [];
 
+    //Start empty
+    this.items = []
+
+    //Add in player items
     window.playerState.items.forEach(item => {
       this.items.push({
         ...item,
-      });
-    });
+        team: "player"
+      })
+    })
 
     this.usedInstanceIds = {};
+
   }
 
-  addCombatant(id, config) {
-    if (!config || !config.name || !config.hp || !config.maxHp || !config.level) {
-      console.error("Invalid config object passed to addCombatant:", config);
-      return;
-    }
+  addCombatant(id, team, config) {
+      this.combatants[id] = new Combatant({
+        ...Pizzas[config.pizzaId],
+        ...config,
+        team,
+        isPlayerControlled: team === "player"
+      }, this)
 
-    this.combatants[id] = new Combatant({
-      name: config.name,
-      hp: config.hp,
-      maxHp: config.maxHp,
-      level: config.level,
-      isPlayerControlled: id === "player"
-    }, this);
+      //Populate first active pizza
 
-    this.activeCombatants[id] = id;
+      console.log(this)
+      this.activeCombatants[team] = this.activeCombatants[team] || id
   }
 
   createElement() {
     this.element = document.createElement("div");
     this.element.classList.add("Battle");
     this.element.innerHTML = (`
-      <div class="Battle_hero">
-        <img src="${'/projekt/images/hero.png'}" alt="Hero" />
-      </div>
-      <div class="Battle_enemy">
-        <img src="${this.enemy.src}" alt="${this.enemy.name}" />
-      </div>
-    `);
+    <div class="Battle_hero">
+      <img src="${'/images/characters/people/hero.png'}" alt="Hero" />
+    </div>
+    <div class="Battle_enemy">
+      <img src=${this.enemy.src} alt=${this.enemy.name} />
+    </div>
+    `)
   }
 
   init(container) {
     this.createElement();
     container.appendChild(this.element);
 
+    this.playerTeam = new Team("player", "Hero");
+    this.enemyTeam = new Team("enemy", "Bully");
+
     Object.keys(this.combatants).forEach(key => {
       let combatant = this.combatants[key];
       combatant.id = key;
-      combatant.init(this.element);
-    });
+      combatant.init(this.element)
+      
+      //Add to correct team
+      if (combatant.team === "player") {
+        this.playerTeam.combatants.push(combatant);
+      } else if (combatant.team === "enemy") {
+        this.enemyTeam.combatants.push(combatant);
+      }
+    })
+
+    this.playerTeam.init(this.element);
+    this.enemyTeam.init(this.element);
 
     this.turnCycle = new TurnCycle({
       battle: this,
       onNewEvent: event => {
         return new Promise(resolve => {
-          const battleEvent = new BattleEvent(event, this);
+          const battleEvent = new BattleEvent(event, this)
           battleEvent.init(resolve);
-        });
+        })
       },
       onWinner: winner => {
+
         if (winner === "player") {
           const playerState = window.playerState;
-          playerState.hp = this.combatants["player"].hp;
+          Object.keys(playerState.pizzas).forEach(id => {
+            const playerStatePizza = playerState.pizzas[id];
+            const combatant = this.combatants[id];
+            if (combatant) {
+              playerStatePizza.hp = combatant.hp;
+              playerStatePizza.xp = combatant.xp;
+              playerStatePizza.maxXp = combatant.maxXp;
+              playerStatePizza.level = combatant.level;
+            }
+          })
 
-          window.playerStats.hp = playerState.hp;
-          window.playerStats.level = this.combatants["player"].level;
+          //Get rid of player used items
+          playerState.items = playerState.items.filter(item => {
+            return !this.usedInstanceIds[item.instanceId]
+          })
+
+
         }
 
         this.element.remove();
         this.onComplete();
       }
-    });
+    })
     this.turnCycle.init();
+
+
   }
+
 }
--- battle/BattleEvent.js
+++ battle/BattleEvent.js
@@ -5,32 +5,33 @@ class BattleEvent {
   }
   
   textMessage(resolve) {
+
     const text = this.event.text
-      .replace("{CASTER}", this.event.caster?.name)
-      .replace("{TARGET}", this.event.target?.name)
-      .replace("{ACTION}", this.event.action?.name);
+    .replace("{CASTER}", this.event.caster?.name)
+    .replace("{TARGET}", this.event.target?.name)
+    .replace("{ACTION}", this.event.action?.name)
 
     const message = new TextMessage({
       text,
       onComplete: () => {
         resolve();
       }
-    });
-    message.init(this.battle.element);
+    })
+    message.init( this.battle.element )
   }
 
   async stateChange(resolve) {
-    const { caster, target, damage, recover, status } = this.event;
+    const {caster, target, damage, recover, status, action} = this.event;
     let who = this.event.onCaster ? caster : target;
 
     if (damage) {
-      // Modify the target to have less HP
+      //modify the target to have less HP
       target.update({
         hp: target.hp - damage
-      });
+      })
       
-      // Start blinking
-      target.element.classList.add("battle-damage-blink");
+      //start blinking
+      target.pizzaElement.classList.add("battle-damage-blink");
     }
 
     if (recover) {
@@ -40,55 +41,92 @@ class BattleEvent {
       }
       who.update({
         hp: newHp
-      });
+      })
     }
 
     if (status) {
       who.update({
-        status: { ...status }
-      });
+        status: {...status}
+      })
     }
     if (status === null) {
       who.update({
         status: null
-      });
+      })
     }
 
-    // Wait a little bit
-    await utils.wait(600);
 
-    // Update components
-    this.battle.combatants["player"].update();
-    this.battle.combatants["enemy"].update();
+    //Wait a little bit
+    await utils.wait(600)
 
-    // Stop blinking
-    target.element.classList.remove("battle-damage-blink");
+    //Update Team components
+    this.battle.playerTeam.update();
+    this.battle.enemyTeam.update();
+
+    //stop blinking
+    target.pizzaElement.classList.remove("battle-damage-blink");
     resolve();
   }
 
   submissionMenu(resolve) {
-    const { caster } = this.event;
+    const {caster} = this.event;
     const menu = new SubmissionMenu({
       caster: caster,
       enemy: this.event.enemy,
       items: this.battle.items,
+      replacements: Object.values(this.battle.combatants).filter(c => {
+        return c.id !== caster.id && c.team === caster.team && c.hp > 0
+      }),
       onComplete: submission => {
-        // submission { what move to use, who to use it on }
-        resolve(submission);
+        //submission { what move to use, who to use it on }
+        resolve(submission)
       }
-    });
-    menu.init(this.battle.element);
+    })
+    menu.init( this.battle.element )
+  }
+
+  replacementMenu(resolve) {
+    const menu = new ReplacementMenu({
+      replacements: Object.values(this.battle.combatants).filter(c => {
+        return c.team === this.event.team && c.hp > 0
+      }),
+      onComplete: replacement => {
+        resolve(replacement)
+      }
+    })
+    menu.init( this.battle.element )
+  }
+
+  async replace(resolve) {
+    const {replacement} = this.event;
+
+    //Clear out the old combatant
+    const prevCombatant = this.battle.combatants[this.battle.activeCombatants[replacement.team]];
+    this.battle.activeCombatants[replacement.team] = null;
+    prevCombatant.update();
+    await utils.wait(400);
+
+    //In with the new!
+    this.battle.activeCombatants[replacement.team] = replacement.id;
+    replacement.update();
+    await utils.wait(400);
+
+    //Update Team components
+    this.battle.playerTeam.update();
+    this.battle.enemyTeam.update();
+
+    resolve();
   }
 
   giveXp(resolve) {
     let amount = this.event.xp;
-    const { combatant } = this.event;
+    const {combatant} = this.event;
     const step = () => {
       if (amount > 0) {
         amount -= 1;
         combatant.xp += 1;
 
-        // Check if we've hit level up point
+        //Check if we've hit level up point
         if (combatant.xp === combatant.maxXp) {
           combatant.xp = 0;
           combatant.maxXp = 100;
