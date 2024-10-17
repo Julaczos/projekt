d29aab7bbfb6ccb3cb7adc7abadf19735a50cca4
Julaczos <julaczosoo@gmail.com>
2024-10-17 13:52:10 +0200

    123

--- battle/Battle.js
+++ battle/Battle.js
@@ -1,49 +1,39 @@
 class Battle {
-  constructor({ enemy, onComplete }) {
-
-    this.enemy = enemy;
-    this.onComplete = onComplete;
-
+  constructor() {
     this.combatants = {
-    }
-
-    this.activeCombatants = {
-      player: null, 
-      enemy: null, 
-    }
-
-    window.playerState.lineup.forEach(id => {
-      this.addCombatant(id, "player", window.playerState.pizzas[id])
-    });
-    Object.keys(this.enemy.pizzas).forEach(key => {
-      this.addCombatant("e_"+key, "enemy", this.enemy.pizzas[key])
-    })
-
-
-    this.items = []
-
-    window.playerState.items.forEach(item => {
-      this.items.push({
-        ...item,
-        team: "player"
-      })
-    })
-
-    this.usedInstanceIds = {};
-
-  }
-
-  addCombatant(id, team, config) {
-      this.combatants[id] = new Combatant({
-        ...Pizzas[config.pizzaId],
-        ...config,
-        team,
-        isPlayerControlled: team === "player"
+      "player1": new Combatant({
+        ...Pizzas.s001,
+        team: "player",
+        hp: 30,
+        maxHp: 50,
+        xp: 75,
+        maxXp: 100,
+        level: 1,
+        status: null
+      }, this),
+      "enemy1": new Combatant({
+        ...Pizzas.v001,
+        team: "enemy",
+        hp: 20,
+        maxHp: 50,
+        xp: 20,
+        maxXp: 100,
+        level: 1,
+      }, this),
+      "enemy2": new Combatant({
+        ...Pizzas.f001,
+        team: "enemy",
+        hp: 25,
+        maxHp: 50,
+        xp: 30,
+        maxXp: 100,
+        level: 1,
       }, this)
-
-
-      console.log(this)
-      this.activeCombatants[team] = this.activeCombatants[team] || id
+    }
+    this.activeCombatants = {
+      player: "player1",
+      enemy: "enemy1",
+    }
   }
 
   createElement() {
@@ -54,7 +44,7 @@ class Battle {
       <img src="${'/images/characters/people/hero.png'}" alt="Hero" />
     </div>
     <div class="Battle_enemy">
-      <img src=${this.enemy.src} alt=${this.enemy.name} />
+      <img src=${'/images/characters/people/npc3.png'} alt="Enemy" />
     </div>
     `)
   }
@@ -63,60 +53,11 @@ class Battle {
     this.createElement();
     container.appendChild(this.element);
 
-    this.playerTeam = new Team("player", "Hero");
-    this.enemyTeam = new Team("enemy", "Bully");
-
     Object.keys(this.combatants).forEach(key => {
       let combatant = this.combatants[key];
       combatant.id = key;
       combatant.init(this.element)
-      
-      if (combatant.team === "player") {
-        this.playerTeam.combatants.push(combatant);
-      } else if (combatant.team === "enemy") {
-        this.enemyTeam.combatants.push(combatant);
-      }
     })
-
-    this.playerTeam.init(this.element);
-    this.enemyTeam.init(this.element);
-
-    this.turnCycle = new TurnCycle({
-      battle: this,
-      onNewEvent: event => {
-        return new Promise(resolve => {
-          const battleEvent = new BattleEvent(event, this)
-          battleEvent.init(resolve);
-        })
-      },
-      onWinner: winner => {
-
-        if (winner === "player") {
-          const playerState = window.playerState;
-          Object.keys(playerState.pizzas).forEach(id => {
-            const playerStatePizza = playerState.pizzas[id];
-            const combatant = this.combatants[id];
-            if (combatant) {
-              playerStatePizza.hp = combatant.hp;
-              playerStatePizza.xp = combatant.xp;
-              playerStatePizza.maxXp = combatant.maxXp;
-              playerStatePizza.level = combatant.level;
-            }
-          })
-
-          playerState.items = playerState.items.filter(item => {
-            return !this.usedInstanceIds[item.instanceId]
-          })
-
-
-        }
-
-        this.element.remove();
-        this.onComplete();
-      }
-    })
-    this.turnCycle.init();
-
 
   }
 
