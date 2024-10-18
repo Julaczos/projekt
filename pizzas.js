289a4cb853f0dfa3976f3ca9f988c531306d1282
Julaczos <julaczosoo@gmail.com>
2024-10-18 14:59:25 +0200

    sprawdzam

--- pizzas.js
+++ pizzas.js
@@ -10,24 +10,28 @@ window.PizzaTypes = {
     "s001": {
       name: "Slice Samurai",
       description: "Pizza desc here",
+      src: "/projekt/images/pizzaPicture.png",
       type: PizzaTypes.spicy,
       actions: [ "saucyStatus", "clumsyStatus", "damage1" ],
     },
     "s002": {
       name: "Bacon Brigade",
       description: "A salty warrior who fears nothing",
+      src: "/projekt/images/pizzaPicture.png",
       type: PizzaTypes.spicy,
       actions: [ "damage1", "saucyStatus", "clumsyStatus" ],
     },
     "v001": {
       name: "Call Me Kale",
       description: "Pizza desc here",
+      src: "/projekt/images/pizzaPicture.png",
       type: PizzaTypes.veggie,
       actions: [ "damage1" ],
     },
     "f001": {
       name: "Portobello Express",
       description: "Pizza desc here",
+      src: "/projekt/images/pizzaPicture.png",
       type: PizzaTypes.fungi,
       actions: [ "damage1" ],
     }
