16a506cdf4ddb4195f8b6f67caca2e9ccda9f8b2
Julaczos <julaczosoo@gmail.com>
2024-10-22 23:34:18 +0200

    Update OverworldEvent.js

--- OverworldEvent.js
+++ OverworldEvent.js
@@ -1,18 +1,18 @@
-class OverworldEvent {
+OverworldEvent {
   constructor({ map, event}) {
     this.map = map;
     this.event = event;
   }
 
   stand(resolve) {
-    const who = this.map.gameObjects[this.event.who];
+    const who = this.map.gameObjects[ this.event.who ];
     who.startBehavior({
       map: this.map
     }, {
       type: "stand",
       direction: this.event.direction,
       time: this.event.time
-    });
+    })
     
     const completeHandler = e => {
       if (e.detail.whoId === this.event.who) {
@@ -20,7 +20,67 @@ class OverworldEvent {
         resolve();
       }
     }
-    document.addEventListener("PersonStandComplete", completeHandler);
+    document.addEventListener("PersonStandComplete", completeHandler)
+  }
+
+  walk(resolve) {
+    const who = this.map.gameObjects[ this.event.who ];
+    who.startBehavior({
+      map: this.map
+    }, {
+      type: "walk",
+      direction: this.event.direction,
+      retry: true
+    })
+
+    const completeHandler = e => {
+      if (e.detail.whoId === this.event.who) {
+        document.removeEventListener("PersonWalkingComplete", completeHandler);
+        resolve();
+      }
+    }
+    document.addEventListener("PersonWalkingComplete", completeHandler)
+
+  }
+
+  textMessage(resolve) {
+
+    if (this.event.faceHero) {
+      const obj = this.map.gameObjects[this.event.faceHero];
+      obj.direction = utils.oppositeDirection(this.map.gameObjects["hero"].direction);
+    }
+
+    const message = new TextMessage({
+      text: this.event.text,
+      onComplete: () => resolve()
+    })
+    message.init( document.querySelector(".game-container") )
+  }
+  
+  async changeMap(resolve) {
+    this.map.overworld.startMap( window.OverworldMaps[this.event.map], {
+      x: this.event.x,
+      y: this.event.y,
+      direction: this.event.direction,
+    });
+    updateLocation(this.event.map); 
+    await checkLocationAndStartCamera();
+    resolve();
+
+  }
+  
+  battle(resolve) {
+    console.log("Enemy ID:", this.event.enemyId);
+    console.log("Enemy Object:", Enemies[this.event.enemyId]);
+    
+    const battle = new Battle({
+      enemy: Enemies[this.event.enemyId],
+      onComplete: () => {
+        resolve();
+      }
+    })
+    battle.init(document.querySelector(".game-container"));
+
   }
 
   addItem(resolve) {
@@ -33,42 +93,22 @@ class OverworldEvent {
     resolve();
   }
 
-  walk(resolve) {
-    const who = this.map.gameObjects[this.event.who];
-    who.startBehavior({
-      map: this.map
-    }, {
-      type: "walk",
-      direction: this.event.direction,
-      retry: true
-    });
-
-    const completeHandler = e => {
-      if (e.detail.whoId === this.event.who) {
-        document.removeEventListener("PersonWalkingComplete", completeHandler);
+  pause(resolve) {
+    this.map.isPaused = true;
+    const menu = new PauseMenu({
+      progress: this.map.overworld.progress,
+      onComplete: () => {
         resolve();
+        this.map.isPaused = false;
+        this.map.overworld.startGameLoop();
       }
-    }
-    document.addEventListener("PersonWalkingComplete", completeHandler);
-  }
-
-  textMessage(resolve) {
-    if (this.event.faceHero) {
-      const obj = this.map.gameObjects[this.event.faceHero];
-      obj.direction = utils.oppositeDirection(this.map.gameObjects["hero"].direction);
-    }
-
-    const message = new TextMessage({
-      text: this.event.text,
-      onComplete: () => resolve()
     });
-    message.init(document.querySelector(".game-container"));
+    menu.init(document.querySelector(".game-container"));
   }
 
-  
   init() {
     return new Promise(resolve => {
-      this[this.event.type](resolve);
-    });
+      this[this.event.type](resolve)      
+    })
   }
-}
+
