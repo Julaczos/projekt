9f26e17a7a27e7c45dd8924b10ed1c3c9efb05ca
Julaczos <julaczosoo@gmail.com>
2024-10-22 23:24:35 +0200

    Update PlayerState.js

--- PlayerState.js
+++ PlayerState.js
@@ -15,7 +15,7 @@ class PlayerState {
       { actionId: "item_recoverHp", instanceId: "item1" },
       { actionId: "item_recoverHp", instanceId: "item2" },
       { actionId: "item_recoverHp", instanceId: "item3" },
-    ]
+    ];
     this.squatCount = 0; 
     this.bicepCurlCount = 0; 
     this.storyFlags = {
@@ -34,6 +34,12 @@ class PlayerState {
     this.pizzas.p1.level++;
     this.pizzas.p1.xp -= this.pizzas.p1.maxXp;
     this.pizzas.p1.maxXp = Math.floor(this.pizzas.p1.maxXp * 1.5);
+  }
+
+  addItem(itemId) {
+    const newItem = { actionId: itemId, instanceId: `item${this.items.length + 1}` };
+    this.items.push(newItem);
+    console.log(`Dodano przedmiot: ${itemId}`);
   }
 }
 window.playerState = new PlayerState();
